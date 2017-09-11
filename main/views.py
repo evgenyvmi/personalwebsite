ba1e188ba1a366966c8a6d769ecd3a002d0ffbe2
evgeny <anruevinsnew@gmail.com>
2017-09-11 23:46:51 +0500

    i've done something

--- main/views.py
+++ main/views.py
@@ -11,21 +11,38 @@ def main_page(request):
 
 def blog(request):
 	if request.method == 'POST':
-		print('lol')
-	blog_posts = blogPost.objects.all()
-	blog_posts = [blog_posts[0], blog_posts[1], blog_posts[2]]
-	pages = 0
+		pass
+	blog_posts = blogPost.objects.order_by('-date')
+	blog_posts = blog_posts[:3]
+	page = 0
 	print(blog_posts)
-	return render(request, 'Main/blog.html', {'blog_posts': blog_posts, 'pages': pages})
+	return render(request, 'Main/blog.html', {'blog_posts': blog_posts, 'page': page})
+
+def blog_page(request, page):
+	if request.method == 'POST':
+		pass
+	blog_posts = blogPost.objects.order_by('-date')
+	page = int(page)
+	blog_posts = blog_posts[page*3:page*3+3]
+	for post in blog_posts:
+		print(post.title)
+	return render(request, 'Main/blog.html', {'blog_posts': blog_posts, 'page': page})	
+
 def blog_create(request):
 	if request.method == 'POST':
 		form = blogPostForm(request.POST, request.FILES)
 		if form.is_valid():
-			print('hello')
 			new_post = form.save()
 			return HttpResponseRedirect('/')
 	form = blogPostForm()
 	return render(request, 'Main/blog_create.html', {'form': form})
+
+def blog_edit(request, post_id):
+	post = blogPost.objects.get(id= post_id)
+	if request.method == 'POST':
+		pass
+	form = blogPostForm(initial={'title': post.title, 'description': post.description})
+	return render(request, 'Main/blog_edit.html', {'form': form})
 
 def music(request):
 	return render(request, 'Main/music.html')
